f9a1f51ed21e60992cf0827c7cda8ddde9b34331
Travis <travismay@gmail.com>
2014-05-19 13:55:38 -0500

    Modify download.R

--- download.R
+++ download.R
@@ -2,14 +2,12 @@
 ####  Returns a location of the data file to be processed by the createTable() function.
 ####  Function will not run if a directory of data previously exists.
 
-download <- function(url, dataFileName, zipFileName) {
-  
-  #   url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-  #   zipFileName <- "national_emissions.zip"
+download <- function(url) {
   
   ## Establish destinations for zip and data files.
   dataDirPath <- "data"
-  datasetDirPath <- file.path(dataDirPath, dataFileName)
+  datasetDirPath <- file.path(dataDirPath, "national_emissions")
+  zipFileName <- "national_emissions.zip"
   zipPath <- file.path(dataDirPath, zipFileName)
   
   ## Downloading/Unzipping data *iff* data doesn't already exist
@@ -17,27 +15,19 @@ download <- function(url, dataFileName, zipFileName) {
     dir.create(dataDirPath) 
   }
   
-  ## Validate if compressed file already exists.
+  ## Validate whether compressed file already exists.
   if(!file.exists(zipPath)){ 
     download.file(url, zipPath, mode="wb")
   }
   
-  ## Validate if decompressed file exists in dir.
+  ## Validate whether decompressed file exists in dir.
   if(!file.exists(datasetDirPath)) { 
     unzip(zipPath, exdir=dataDirPath)
   }
   
-  return(datasetDirPath)
+#   return(datasetDirPath)
   
 }
 
-createTable <- function(file) {
-  ##  Read in file, but only rows from 2007-02-01 through 2007-02-02.
-  data <- readRDS(file)
-  data <- na.omit(data)
-  
 
-  
-  ##  Return modified data set.
-  return(data)
-}
+
