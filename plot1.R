4ff6b951f91f3314c3b8ab1b08e28a208f72a400
Travis <travismay@gmail.com>
2014-05-19 13:55:57 -0500

    Modify plot1.R to aggregate NEI data

--- plot1.R
+++ plot1.R
@@ -1,8 +1,22 @@
+#### Assignment:
+#### Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+#### Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+
+## Source data.table library and download.R functions
 library(data.table)
 source("download.R")
 
+## Set variables for download() function parameters.
   url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
-  dataFileName <- "national_emissions"
-  zipFileName <- "national_emissions.zip"
 
-data <- download(url, dataFileName, zipFileName)
+download(url)
+
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+total_emissions <- aggregate.data.frame(NEI$Emissions, "sum", by=list(NEI$year))
+total_emissions$x <- total_emissions$x * .00001
+
+# png(filename="plot1.png", width=480, height=480)
+with(total_emissions, plot(Group.1, x, type ="b"))
+# dev.off()
