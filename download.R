c062db0dc781dd508b6e2094c5ca9d4100cd9c8b
Travis <travismay@gmail.com>
2014-05-19 10:03:11 -0500

    Modify download.R to read RDS data files

--- download.R
+++ download.R
@@ -2,13 +2,14 @@
 ####  Returns a location of the data file to be processed by the createTable() function.
 ####  Function will not run if a directory of data previously exists.
 
-download <- function(url, zipFileName) {
+download <- function(url, dataFileName, zipFileName) {
   
   #   url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
   #   zipFileName <- "national_emissions.zip"
   
   ## Establish destinations for zip and data files.
   dataDirPath <- "data"
+  datasetDirPath <- file.path(dataDirPath, dataFileName)
   zipPath <- file.path(dataDirPath, zipFileName)
   
   ## Downloading/Unzipping data *iff* data doesn't already exist
@@ -28,4 +29,15 @@ download <- function(url, zipFileName) {
   
   return(datasetDirPath)
   
+}
+
+createTable <- function(file) {
+  ##  Read in file, but only rows from 2007-02-01 through 2007-02-02.
+  data <- readRDS(file)
+  data <- na.omit(data)
+  
+
+  
+  ##  Return modified data set.
+  return(data)
 }
