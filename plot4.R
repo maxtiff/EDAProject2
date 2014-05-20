9484425a43283efc914df10a148f7c07b232bd27
Travis <travismay@gmail.com>
2014-05-20 16:12:56 -0500

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,17 @@
+#### Assignment
+#### Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
+
+## Source data.table library and download.R functions
+library(data.table)
+library(ggplot2)
+source("download.R")
+
+## Set variables for download() function parameters.
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+
+download(url)
+
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+
