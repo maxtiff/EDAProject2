11ba618703d3d6d67e56612a79f85231e11ae42b
Travis <travismay@gmail.com>
2014-05-20 15:50:41 -0500

    Modify plot3.R with more efficient code

--- plot3.R
+++ plot3.R
@@ -7,3 +7,22 @@ library(ggplot2)
 library(data.table)
 library(reshape2)
 source("download.R")
+
+## Set variables for download() function parameters.
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+
+download(url)
+
+## Read in NEI data
+NEI <- readRDS("data/summarySCC_PM25.rds")
+
+## Subset data for Baltimore City only.
+NEI <- NEI[NEI$fips %in% c("24510"),]
+
+## Plot graph with ggplot. Types are seperated into individual facets.
+g <- ggplot(NEI, aes(year, Emissions))
+g + geom_line(aes( y = Emissions ) , stat="summary", fun.y="sum" ) + facet_grid(. ~ type)
+  
+
+
+
