c735631170c156d039d2b46f56b38924d501bcd4
Travis <travismay@gmail.com>
2014-05-20 11:58:30 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,9 @@
+#### Assignment
+#### Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999-2008 for Baltimore City? 
+#### Which have seen increases in emissions from 1999-2008? 
+#### Use the ggplot2 plotting system to make a plot answer this question.
+
+library(ggplot2)
+library(data.table)
+library(reshape2)
+source("download.R")
