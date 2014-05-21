1aaf8cd98570c51ceedec1afc71348adf9c70cbc
Travis <travismay@gmail.com>
2014-05-21 16:19:58 -0500

    Create plot4.PNG from modified plot4.R

--- plot4.R
+++ plot4.R
@@ -14,5 +14,10 @@ download(url)
 NEI <- readRDS("data/summarySCC_PM25.rds")
 SCC <- readRDS("data/Source_Classification_Code.rds")
 
+SCC_coal_comb <- SCC[grepl("coal", SCC$SCC.Level.Three, ignore.case=TRUE) | grepl("Lignite", SCC$SCC.Level.Three, ignore.case=TRUE),]
+NEI_coal_comb <- subset(NEI, SCC %in% SCC_coal_comb$SCC)
 
-
+png(filename="plot4.png", width=480, height=480)
+g <- ggplot(NEI_coal_comb, aes(year, Emissions))
+g + geom_line(aes( y = Emissions ) , stat="summary", fun.y="sum",colour = "red", size = 1 )
+dev.off()
